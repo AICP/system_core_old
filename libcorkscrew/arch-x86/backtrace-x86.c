823c3f4c43860fee0d1272d8cbd1600eb6ef488f
Brandon McAnsh <brandon.mcansh@gmail.com>
2013-06-26 15:37:26 -0700

    libcorkscrew: Fix merge errors

--- libcorkscrew/arch-x86/backtrace-x86.c
+++ libcorkscrew/arch-x86/backtrace-x86.c
@@ -88,8 +88,6 @@ typedef struct ucontext {
 
 #else
 
-// glibc has its own renaming of the Linux kernel's structures.
-#define __USE_GNU // For REG_EBP, REG_ESP, and REG_EIP.
 #include <ucontext.h>
 
 #endif
