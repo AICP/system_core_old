a8b17ed89d7de817114119089af10ac2e36d84de
Whitehawkx <xxwhitehawk@gmail.com>
2013-12-08 13:42:41 -0800

    Revert "TEMP Revert "properties.h: avoid duplicating constant values""
    This reverts commit a10d409ca64d1f63ccbe86ba4cda3333be09ce19.

--- include/cutils/properties.h
+++ include/cutils/properties.h
@@ -19,6 +19,7 @@
 
 #include <sys/cdefs.h>
 #include <stddef.h>
+#include <sys/system_properties.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -31,8 +32,8 @@ extern "C" {
 ** WARNING: system/bionic/include/sys/system_properties.h also defines
 **          these, but with different names.  (TODO: fix that)
 */
-#define PROPERTY_KEY_MAX   32
-#define PROPERTY_VALUE_MAX  92
+#define PROPERTY_KEY_MAX   PROP_NAME_MAX
+#define PROPERTY_VALUE_MAX  PROP_VALUE_MAX
 
 /* property_get: returns the length of the value which will never be
 ** greater than PROPERTY_VALUE_MAX - 1 and will always be zero terminated.
