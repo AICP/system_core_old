14c204f792bcf76d3e4949ea1a81acac425b0d2d
Miccia <bono.michele94@gmail.com>
2017-06-06 09:45:06 +0200

    core: fix derp
    Change-Id: I1994144d96005a2acbba3d9c09f6f81644a6412f

--- libcutils/sched_policy.c
+++ libcutils/sched_policy.c
@@ -130,7 +130,7 @@ static void __initialize(void) {
 #ifdef USE_SCHEDBOOST
     if (!access("/dev/stune/tasks", F_OK)) {
         char* filename;
-	filename = "/dev/stune/top-app/tasks";
+        filename = "/dev/stune/top-app/tasks";
         ta_schedboost_fd = open(filename, O_WRONLY | O_CLOEXEC);
         filename = "/dev/stune/foreground/tasks";
         fg_schedboost_fd = open(filename, O_WRONLY | O_CLOEXEC);
