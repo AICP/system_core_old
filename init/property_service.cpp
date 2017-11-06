69dcaea4d803c3dee558d577da41e5879340b477
Park Ju Hyung <qkrwngud825@gmail.com>
2017-11-06 20:30:39 +0900

    init: workaround SafetyNet check
    Doing this in the userspace allows more properties to be spoofed
    and eliminate the needs for a hack in the kernel.
    
    Change-Id: I25230b8e14dbc7ced02451dd8dfb03848452461c
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -745,6 +745,48 @@ static void load_override_properties() {
     }
 }
 
+/* From Magisk@jni/magiskhide/hide_utils.c */
+static const char *snet_prop_key[] = {
+	"ro.boot.vbmeta.device_state",
+	"ro.boot.verifiedbootstate",
+	"ro.boot.flash.locked",
+	"ro.boot.selinux",
+	"ro.boot.veritymode",
+	"ro.boot.warranty_bit",
+	"ro.warranty_bit",
+	"ro.debuggable",
+	"ro.secure",
+	"ro.build.type",
+	"ro.build.tags",
+	"ro.build.selinux",
+	NULL
+};
+
+static const char *snet_prop_value[] = {
+	"locked",
+	"green",
+	"1",
+	"enforcing",
+	"enforcing",
+	"0",
+	"0",
+	"0",
+	"1",
+	"user",
+	"release-keys",
+	"1",
+	NULL
+};
+
+static void workaround_snet_properties() {
+	LOG(INFO) << "snet: Hiding sensitive props";
+
+	// Hide all sensitive props
+	for (int i = 0; snet_prop_key[i]; ++i) {
+		property_set(snet_prop_key[i], snet_prop_value[i]);
+	}
+}
+
 /* When booting an encrypted system, /data is not mounted when the
  * property service is started, so any properties stored there are
  * not loaded.  Vold triggers init to load these properties once it
@@ -927,6 +969,9 @@ void property_load_boot_defaults(bool load_debug_prop) {
     if (android::base::GetBoolProperty("ro.persistent_properties.ready", false)) {
         update_sys_usb_config();
     }
+
+    // Workaround SafetyNet
+    workaround_snet_properties();
 
     // Restore the normal property override security after init extension is executed
     weaken_prop_override_security = false;
